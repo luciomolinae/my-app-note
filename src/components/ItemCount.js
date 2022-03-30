6ee5e1ff830ea5c22e56355d71b067527422d81f
Lucio Molina <luzxiob3@gmail.com>
2022-03-30 12:02:48 -0300

    Contador arreglado

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,23 +1,50 @@
 import React,{useState} from 'react'
-const ItemCount = () =>{
-    
-    const [numero, setNumero] = useState(0)
-    const Aumentar = () =>{
-        setNumero(numero + 1)
-    }
 
-    const Reducir = () =>{
-        if (numero > 0){
-        setNumero(numero - 1)
-    }
-    }
-    return(
-        <>
-           <h5> {numero}</h5>
-            <button onClick={Aumentar}>+</button>
-            <button onClick={Reducir}>-</button>
-        </>
+
+const ItemCount = ({stock}) =>{
+
+    const initial = 0;
+    const [cantidad, useContador] = useState(0);
+
+    const ClickSumar =  () => {
+        useContador (cantidad +1);
+    };
+    const ClickRestar = () => {
+        useContador (cantidad -1);
+    };
+
+    return (
+        <div>
+            <button onClick={ClickSumar} disabled={cantidad === stock ? true : false}>+</button>
+            <button onClick={ClickRestar} disabled={cantidad === initial ? true : false}>-</button>
+            <p>Cantidad: {cantidad}</p>
+        </div>
     )
+
+
 }
+
+
+// const ItemCount = () =>{
+    
+//     const [numero, setNumero] = useState(0)
+//     const Aumentar = () =>{
+//         setNumero(numero + 1)
+//     }
+
+//     const Reducir = () =>{
+//         if (numero > 0){
+//         setNumero(numero - 1)
+//     }
+//     }
+//     return(
+//         <>
+//            <h5> {numero}</h5>
+//             <button onClick={Aumentar}>+</button>
+//             <button onClick={Reducir}>-</button>
+//             <button>Comprar</button>
+//         </>
+//     )
+// }
 
 export default ItemCount
